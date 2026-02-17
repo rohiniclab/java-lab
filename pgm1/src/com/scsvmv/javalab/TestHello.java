cf42242c4eeb7bde883b1bccadb00e96dc358263
rohiniclab <rohiniganesh0724@gmail.com>
2026-02-17 02:45:24 -0800

    Add files via upload

---
+++ pgm1/src/com/scsvmv/javalab/TestHello.java
@@ -0,0 +1,25 @@
+package com.scsvmv.javalab;
+import com.scsvmv.javalab.hello.Hello; 
+
+public class TestHello{
+  public static void main(String[] args){
+   Hello simpleHello = new Hello();
+   simpleHello.wish(); 
+
+   com.scsvmv.javalab.hello.Hello h = 
+        new com.scsvmv.javalab.hello.Hello();
+   
+ if(args.length>0){
+    h.wish(args[0]);
+    h.wishWithDate(args[0]);
+}else{
+   System.out.println("Usage:");
+   System.out.println("java com.scsvmv.javalab.TestHello<name>");
+   System.out.println("Example:");
+   System.out.println("java com.scsvmv.javalab.TestHello Student");
+    } 
+  }
+}
+
+
+ 
